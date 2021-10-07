f00e9aeec6f37d87bbca6113bf717b99a5c1d12b
kamilstpn <kamil.stepien.st@gmail.com>
2021-10-07 11:12:42 +0200

    7.10

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users= [
+    {
+        name: 'Adam Romański',
+        attendance: '39%',
+        average: '2.3',
+    },
+    {
+        name: 'Kamil Stepien',
+        attendance: '72%',
+        average: '6.3',
+    },
+    {
+        name: 'Andrzej Kowalewski',
+        attendance: '85%',
+        average: '4.3',
+    },
+    {
+        name: 'Wojciech Tomaszewski',
+        attendance: '59%',
+        average: '4.1',
+    },
+    {
+        name: 'Michał Kubiak',
+        attendance: '21%',
+        average: '2.0',
+    },
+    {
+        name: 'Stanisław Korzeniowski',
+        attendance: '100%',
+        average: '5.3',
+    },
+    {
+        name: 'Michał Wolny',
+        attendance: '11%',
+        average: '2.1',
+    },
+]
