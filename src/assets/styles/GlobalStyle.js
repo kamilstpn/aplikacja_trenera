2ddcafc841116b5d50c5caeba1ef15d7f9a148ce
kamilstpn <kamil.stepien.st@gmail.com>
2021-10-26 14:06:03 +0200

    nawigacja

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -1,20 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-html {
-box-sizing: border-box;
-}
-
-*,*::after, *::before{
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *::after, *::before {
     box-sizing: inherit;
-}
-
-body {
+  }
+  
+  body {
     font-family: 'Montserrat', sans-serif;
-}
-
-a, button {
+    margin: 0;
+    padding: 0;
+  }
+  
+  a, button {
     font-family: 'Montserrat', sans-serif;
-}
+  }
 `;
--- src/components/molecules/UsersListItem/UsersListItem.styles.js
+++ src/components/molecules/UsersListItem/UsersListItem.styles.js
@@ -1,17 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Wrapper=styled.li`
-display: flex;
-align-items: center;
-position: relative;
-&:not(:last-child)::after{
+export const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(:last-child)::after {
     content: '';
     position: absolute;
     bottom: 0;
-    widht: 100%;
+    width: 100%;
     height: 1px;
     background-color: lightgrey;
-}
+  }
 `;
 
 export const StyledAverage = styled.div`
